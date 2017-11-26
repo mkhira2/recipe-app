623f7321930036ea2e65fa1befd0c60766f89e2b
Kenji Hirabayashi <mkhira2@gmail.com>
2017-11-26 01:22:20 -0600

    fixing page reload issues

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,11 +9,11 @@ export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG', [
+        new Recipe('A Test Recipe', 'A super-tasty Schnitzel - just awesome!', 'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG', [
             new Ingredient('Meat', 1),
             new Ingredient('French Fries', 20)
         ]),
-        new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg', [
+        new Recipe('Another Test Recipe', 'What else do you need to say?', 'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg', [
             new Ingredient('Buns', 2),
             new Ingredient('Meat', 1)
         ])
